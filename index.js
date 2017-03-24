c74098dbca17ac1ca65ba19762ee0d6f52c05337
ISh- <imwaves@gmail.com>
2017-03-24 18:02:22 +0300

    Webpack 2: getOptions + config

--- index.js
+++ index.js
@@ -2,26 +2,17 @@ var loaderUtils = require("loader-utils");
 
 module.exports = function() {}
 module.exports.pitch = function applyLoader(remainingRequest) {
-  var query = loaderUtils.parseQuery(this.query);
+  var opts = this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : loaderUtils.getOptions(this) || {};
+
   var args = [];
 
   this.cacheable && this.cacheable();
 
-  // apply?config=key => sourceFn(require('webpack.config').key)
-  if (typeof query.config === "string") {
-    if (!(query.config in this.options))
-      throw new Error("apply-loader: '"+query.config+"' property not present in webpack config");
-    args.push(this.options[query.config]);
-  }
-
-  // apply?{obj: {a: 1, b: 2}} => sourceFn({a: 1, b:2})
-  if (query.obj) {
-    args.push(query.obj);
-  }
-
   // apply?args[]=1&args[]=2 => sourceFn(1, 2)
-  if (Array.isArray(query.args)) {
-    args.push.apply(args, query.args);
+  if (Array.isArray(opts.args)) {
+    args.push.apply(args, opts.args);
+  } else if (opts.obj) {
+    args.push(opts.obj);
   }
 
   return [
